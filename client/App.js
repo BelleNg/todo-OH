d849c849f3d094974622de24ed89b3ef3f150de2
Belle Nguyen <NgBelleK@gmail.com>
2020-07-29 14:37:22 -0500

    problem in frontend App.js line 33

---
+++ client/App.js
@@ -0,0 +1,52 @@
+//import react <-- ONly need react
+import React from 'react'
+import axios from 'axios'
+
+
+
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      string: "something is here"
+    }
+    this.clickHandle = this.clickHandle.bind(this);
+  }
+
+  clickHandle() {
+    console.log("button has been clicked");
+    axios.get('/strings')
+    .then((response) => {
+      this.setState({string: response})
+    })
+    .catch( (error) => {
+      console.log(error);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button type="button" onClick={this.clickHandle} >Click Me!</button>
+
+        <div>{this.state.string}</div>
+      </div>
+    )
+
+  }
+
+}
+
+//send get function to server
+//connect button to get response
+
+//receieve from string from server
+//render that string to the page
+
+//make a button and on that event
+  //make a get request
+  //receive the string response
+  //take that response and store it in state.property
+
+
---
+++ client/index.js
@@ -0,0 +1,9 @@
+//import react
+import React from 'react'
+//import reactDOm <-- needs in index.js
+import ReactDOM from 'react-dom' //used together, seperate packages
+//import app.js
+import App from './App.js' //same directory, add dot
+
+ReactDOM.render(<App/>, document.getElementById('root')
+);
